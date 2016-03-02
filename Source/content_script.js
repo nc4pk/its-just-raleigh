7422a6e066f4c3245e17f1377fa99c061d862880
Patrick Knight <pknight@outlook.com>
2016-03-02 14:15:32 -0500

    Make Raleigh Great Again! initial commit

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,47 +1,33 @@
-walk(document.body);
-
-function walk(node) 
-{
+var walk = function(node) {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
 	
 	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
 
-	switch ( node.nodeType )  
-	{
+	switch (node.nodeType) {
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
-			{
+			while (child) {
 				next = child.nextSibling;
 				walk(child);
 				child = next;
 			}
 			break;
-
 		case 3: // Text node
 			handleText(node);
 			break;
 	}
-}
+};
 
-function handleText(textNode) 
-{
+var handleText = function(textNode) {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bRaleigh-Durham\b/g, "Raleigh");
+	v = v.replace(/\braleigh-durham\b/g, "raleigh");
 	
 	textNode.nodeValue = v;
 }
 
-
+walk(document.body);
